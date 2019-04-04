771969b2db6c8de04445b6a0aa6edddebf7a14a8
AnnaLaine <laine.anna@outlook.com>
2019-04-04 17:54:37 +0300

    lisättiin sovelluslogiikkaan toimintaa sisäänkirjautumiseen ja käyttäjän luontiin

--- CaressysApp/src/main/java/caressys/domain/caressysService.java
+++ CaressysApp/src/main/java/caressys/domain/caressysService.java
@@ -1,14 +1,53 @@
 
 package caressys.domain;
 
+import caressys.dao.UserDao;
+
 public class caressysService {
+    private UserDao userDao;
+    private User loggedInUser;
     
-    
-    public caressysService() {
-        
+    public caressysService(UserDao userDao) {
+        this.userDao = userDao;
     }
     
-    public void createUser(String username, String name) {
-        User newUser = new User(name, username);
+    /*
+    loggin in by a given username. 
+    Find the parameter username from userDao. If it doesn't
+    exist, return false, otherwise return true (username exists and
+    is able to log in the app).
+    */
+    public boolean login(String username) {
+        User user = userDao.findByUsername(username);
+        if (user == null) return false;
+        loggedInUser = user;
+        return true;
+    }
+    
+    /*
+    log out from the app
+    */
+    
+    public void logout() {
+        loggedInUser = null;
+    }
+    
+    /*
+    for creating a new user. 
+    The method will return true, if a new user has been created
+    succesfully, otherwise it will return false (for example, if
+    the username is already taken or there is an exception while 
+    creating a new user.
+    */
+    public boolean createUser(String username, String name) {
+        if (userDao.findByUsername(username) != null) return false;
+        
+        User user = new User(username, name);
+        try {
+            userDao.create(user);
+        } catch(Exception e) {
+            return false;
+        }
+        return true;
     }
 }
