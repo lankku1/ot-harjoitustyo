6f36116221711a0bb29d0dd6b784230d9a2cf454
AnnaLaine <laine.anna@outlook.com>
2019-04-04 19:45:27 +0300

    tehtiin muutoksia userDao käyttöliittymään

--- CaressysApp/src/main/java/caressys/dao/UserDao.java
+++ CaressysApp/src/main/java/caressys/dao/UserDao.java
@@ -1,14 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package caressys.dao;
 
-/**
- *
- * @author lankku
- */
+import caressys.domain.User;
+import java.util.List;
+
 public interface UserDao {
     
+    User create(User user) throws Exception;
+
+    User findByUsername(String username);
+
+    List<User> getAll();
 }
