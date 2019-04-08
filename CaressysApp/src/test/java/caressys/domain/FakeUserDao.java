9bd883b924c4008fbdfcc68adba1f0eb439aa13a
AnnaLaine <laine.anna@outlook.com>
2019-04-08 16:27:51 +0300

    luotiin userDao luokka testejä varten

--- CaressysApp/src/test/java/caressys/domain/FakeUserDao.java
+++ CaressysApp/src/test/java/caressys/domain/FakeUserDao.java
@@ -1,14 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package caressys.domain;
 
-/**
- *
- * @author lankku
- */
-public class FakeUserDao {
+import caressys.dao.UserDao;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FakeUserDao implements UserDao {
+    ArrayList<User> users = new ArrayList<>();
+
+    public FakeUserDao() {
+        users.add(new User("muumi", "Maija Meikäläinen"));
+    }
+    
+    @Override
+    public User findByUsername(String username) {
+        return users.stream().filter(u->u.getUsername().equals(username)).findFirst().orElse(null);
+    }
+    
+    @Override
+    public User create(User user) {
+        users.add(user);
+        return user;
+    } 
+    
+    @Override
+    public List<User> getAll() {
+        return users;
+    }
     
 }
