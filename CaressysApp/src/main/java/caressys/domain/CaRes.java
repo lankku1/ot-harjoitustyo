b9ba2530f73581bbe1f3428df3ebf362efa03ed6
AnnaLaine <laine.anna@outlook.com>
2019-04-11 19:24:35 +0300

    luotiin CaRes luokka varauksia varten

---
+++ CaressysApp/src/main/java/caressys/domain/CaRes.java
@@ -0,0 +1,17 @@
+
+package caressys.domain;
+
+import java.util.Date;
+
+/*
+For a single reservation
+*/
+
+public class CaRes {
+    private int id;
+    private Date arrivalDate;
+    private Date departureDate;
+    private User user;
+    
+    public CaRes
+}
