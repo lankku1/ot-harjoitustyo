aa5015d0b56c2920fd71df654e291a46d2361307
AnnaLaine <laine.anna@outlook.com>
2019-04-15 15:13:06 +0300

    muokattiin create metodin nimeä

--- CaressysApp/src/main/java/caressys/dao/CaresDao.java
+++ CaressysApp/src/main/java/caressys/dao/CaresDao.java
@@ -2,6 +2,7 @@
 package caressys.dao;
 
 import caressys.domain.Cares;
+import java.time.LocalDate;
 import java.util.List;
 
 public interface CaresDao {
@@ -9,4 +10,6 @@ public interface CaresDao {
     Cares create(Cares res) throws Exception;
 
     List<Cares> getAll();
+    
+    LocalDate findByDate(LocalDate date);
 }
