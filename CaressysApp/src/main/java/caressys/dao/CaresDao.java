1f3a30f59881c836949402c4c50e18ad7152ce91
AnnaLaine <laine.anna@outlook.com>
2019-04-15 14:24:09 +0300

    lisättiin findByDate metodi

--- CaressysApp/src/main/java/caressys/dao/CaresDao.java
+++ CaressysApp/src/main/java/caressys/dao/CaresDao.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package caressys.dao;
 
-/**
- *
- * @author lankku
- */
+import caressys.domain.Cares;
+import java.util.List;
+
 public interface CaresDao {
     
+    Cares create(Cares res) throws Exception;
+
+    List<Cares> getAll();
 }
