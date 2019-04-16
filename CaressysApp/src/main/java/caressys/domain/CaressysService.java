489f415fd0de72899a218208cb18d13ac39f1ffd
AnnaLaine <laine.anna@outlook.com>
2019-04-16 14:38:07 +0300

    luotiin toiminnot varauksen luonnille ja listaamiselle

--- CaressysApp/src/main/java/caressys/domain/CaressysService.java
+++ CaressysApp/src/main/java/caressys/domain/CaressysService.java
@@ -1,14 +1,21 @@
 
 package caressys.domain;
 
+import caressys.dao.CaresDao;
 import caressys.dao.UserDao;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class CaressysService {
     private UserDao userDao;
+    private CaresDao caresDao;
     private User loggedInUser;
     
-    public CaressysService(UserDao userDao) {
+    public CaressysService(UserDao userDao, CaresDao caresDao) {
         this.userDao = userDao;
+        this.caresDao = caresDao;
     }
     
     /*
@@ -58,4 +65,39 @@ public class CaressysService {
         }
         return true;
     }
+    
+    
+    /*
+    for creating a new reservation. Returns true if the reservation was
+    created succesfully, otherwise it will return false. Needs an addition
+    for checking, if the reservation is between an already made reservation.
+    */
+    public boolean createReservation(LocalDate arrival, LocalDate departure) {
+        // needs a function for checking if there is a reservation 
+        // made during the timeline given !!
+        
+        Cares reservation = new Cares(0, arrival, departure, loggedInUser);
+        try {
+            caresDao.create(reservation);
+        } catch(Exception e) {
+            return false;
+        }
+        return true;
+    }
+    
+    /*
+    used for listing all the reservation for a specific user
+    */
+    
+    public List<Cares> listAllReservations() {
+        if (loggedInUser == null) {
+            return new ArrayList<>();
+        }
+        
+        return caresDao.getAll()
+                .stream()
+                .filter(t -> t.getUser().equals(loggedInUser))
+                .collect(Collectors.toList());
+    }
+    
 }
