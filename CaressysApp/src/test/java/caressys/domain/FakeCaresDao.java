e7ca9580b23e2a9c1d66c7d9f5b2ac4e602c2031
AnnaLaine <laine.anna@outlook.com>
2019-04-16 15:06:21 +0300

    lisättin luokalle kuvaus

--- CaressysApp/src/test/java/caressys/domain/FakeCaresDao.java
+++ CaressysApp/src/test/java/caressys/domain/FakeCaresDao.java
@@ -1,14 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package caressys.domain;
 
-/**
- *
- * @author lankku
- */
-public class FakeCaresDao {
+import caressys.dao.CaresDao;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FakeCaresDao implements CaresDao { 
+// for test use, so testing won't implement with the actual reservation file
+    ArrayList<Cares> reservations = new ArrayList<>();
+
+    @Override
+    public Cares create(Cares reservation) throws Exception {
+        reservation.setId(reservations.size()+1);
+        reservations.add(reservation);
+        return reservation;
+    }
+
+    @Override
+    public List<Cares> getAll() {
+        return reservations;
+    }
+
+    @Override
+    public LocalDate findByArrivalDate(LocalDate date) {
+        return reservations.stream()
+                .filter(r -> r.getArrival()
+                .equals(date))
+                .findFirst()
+                .orElse(null)
+                .getArrival();
+    }
+
+    @Override
+    public LocalDate findByDepartureDate(LocalDate date) {
+        return reservations.stream()
+                .filter(r -> r.getDeparture()
+                .equals(date))
+                .findFirst()
+                .orElse(null)
+                .getDeparture();
+    }
+
     
 }
