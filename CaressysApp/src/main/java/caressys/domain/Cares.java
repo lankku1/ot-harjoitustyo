57d2ea41e0ac374fa89534feaf870a0933fecc10
AnnaLaine <laine.anna@outlook.com>
2019-04-25 16:27:11 +0300

    lisättiin metodi varauksen keston laskemiselle

--- CaressysApp/src/main/java/caressys/domain/Cares.java
+++ CaressysApp/src/main/java/caressys/domain/Cares.java
@@ -1,6 +1,7 @@
 
 package caressys.domain;
 
+import java.time.Duration;
 import java.time.LocalDate;
 
 /*
@@ -38,6 +39,11 @@ public class Cares { // Calendar Reservation
 
     public int getId() {
         return id;
+    }
+    
+    public Duration getDuration() {
+        Duration duration = Duration.between(arrivalDate, departureDate);
+        return duration;
     }
     
     @Override
